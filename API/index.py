b66a26570bfb69689052a0b99f40212dc83801ae
AlessandroVentisei <alessandro.ventisei@outlook.com>
2024-03-13 12:04:43 +0000

    init todo list

--- API/index.py
+++ API/index.py
@@ -11,3 +11,7 @@ ref = db.reference("/")
 test_data = {"name":"Alex", "purpose": {"test_data":"here", "more_data": "here as well"}}
 ref.set(test_data)
 
+#TODO:
+# 1) setup R package for checking and downloading tiles required for coordinates.
+# 2) upload labelled data returned from R package to real-time db.
+# 3) setup framework for transforming data returned from db in python to useful format.
